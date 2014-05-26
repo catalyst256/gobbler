5e2e806bde203a9a9f53e9e915f26ffc50341a09
Adam Maxwell <catalyst256@gmail.com>
2014-05-26 19:46:22 +0100

    couple of tweaks and added error handling to packet parser

--- uploaders/uploaders.py
+++ uploaders/uploaders.py
@@ -23,19 +23,16 @@ def splunk_shot_udp(splunk_server, splunk_port, s):
     i = re.sub('\w{1,15}={', '', i)
     i = re.sub('HTTP ', '', i)
     i = re.sub('[0-9]{1,2}={\D{5}', '', i)
-    i = i.replace('{', '').replace('}','').replace(': ', '=').replace('\'', '')
+    i = i.replace('{', '').replace('}','').replace(': ', '=').replace('\'', '').replace('\r\n', '')
     sock.send(i)
     sock.close()
-    # sleep(2)
   except Exception, e:
     print e
 
 def splunk_shot_tcp(splunk_server, splunk_port, s):
-  # print GREEN + 'Uploading to Splunk via TCP' + END
   try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((splunk_server, splunk_port))
-    # print GREEN + 'Connected to Splunk server on %s via port %d' %(splunk_server, splunk_port) + END
     i = ', '.join("%s=%r" % (key,val) for (key,val) in s.iteritems())
     i = re.sub('\w{1,15}={', '', i)
     i = re.sub('HTTP ', '', i)
@@ -47,6 +44,5 @@ def splunk_shot_tcp(splunk_server, splunk_port, s):
   sock.close()
 
 def json_dump(s):
-  # print GREEN + 'Outputing to JSON' + END
   t = json.dumps(s, sort_keys=True,indent=2, separators=(',', ': '), ensure_ascii=False, encoding="utf-8")
   print t
